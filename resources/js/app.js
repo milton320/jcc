6056362108ceb0b6567d51e4e58a77ae9371ddbb
milton320 <torrezmilton8@gmail.com>
2022-08-09 22:30:58 -0400

    [Backend/Frontend]integracin con vue3 funcional

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+
+import app from './layouts/app.vue'
+
+createApp(app).mount('#app')
+
---
+++ resources/js/layouts/app.vue
@@ -0,0 +1,6 @@
+
+<template>
+    <div>
+        <h1>HOLA MUNDO DESDE VUEJS3 Y LARAVEL</h1>
+    </div>
+</template>
